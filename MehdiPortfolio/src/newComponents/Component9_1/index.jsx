ff63532cafc04e62b1ce781ae35631c25e916baf
BenAyedMehdi <mehdibenayed97@gmail.com>
2024-04-14 08:49:44 +0200

    add link to about page and change picture

--- MehdiPortfolio/src/newComponents/Component9_1/index.jsx
+++ MehdiPortfolio/src/newComponents/Component9_1/index.jsx
@@ -8,7 +8,7 @@ const Component9_1 = () => {
     <>
       <div className="flex flex-col font-manrope items-center justify-center max-w-[1440px] pl-[120px] pr-[173px] md:px-10 sm:px-5 w-full">
         <div className="flex md:flex-col flex-row md:gap-10 gap-[84px] items-center justify-start max-w-[1147px] mx-auto w-full">
-          <div className="flex flex-1 flex-col gap-8 items-start justify-start w-full">
+          <div className="flex flex-1 flex-col gap-11 items-start justify-start w-full">
             <div className="flex flex-col gap-5 items-start justify-start w-full">
               <Text
                 className="text-4xl sm:text-[32px] md:text-[34px] text-gray-900 tracking-[-0.72px] w-full"
@@ -33,12 +33,14 @@ const Component9_1 = () => {
 
             <div className="bg-white-A700 border border-gray-600 border-solid flex flex-1 flex-col  justify-center sm:px-5 text-base text-center px-6 py-[7px] rounded-[10px] w-full">
               <Button className="bg-white-A700 bottom-[0] cursor-pointer flex items-center justify-center min-w-[122px] px-4 py-[9px] right-[0] rounded-[10px]">
-                <Text
-                  className="text-2xl md:text-[22px] text-center text-gray-900 sm:text-xl tracking-[-0.48px] w-full"
-                  size="txtManropeBold24Gray900"
-                >
-                  More about me
-                </Text>
+                <Link to="/about">
+                  <Text
+                    className="text-2xl md:text-[22px] text-center text-gray-900 sm:text-xl tracking-[-0.48px] w-full"
+                    size="txtManropeBold24Gray900"
+                  >
+                    More about me
+                  </Text>
+                </Link>
               </Button>
             </div>
 
@@ -72,7 +74,7 @@ const Component9_1 = () => {
           <div className="flex md:flex-1 flex-col items-center justify-start w-[47%] md:w-full">
             <Img
               className="flex-1 md:flex-none h-[589px] sm:h-auto max-h-[589px] object-cover rounded-[10px] sm:w-[] md:w-[]"
-              src="mehdi_images/choice.jpg"
+              src="mehdi_images/standing.jpg"
               alt="rectangleTwenty"
             />
           </div>
