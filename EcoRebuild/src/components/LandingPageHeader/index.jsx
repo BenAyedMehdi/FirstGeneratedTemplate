2b528f62903f179e55823c438ccad96e17ddaeda
BenAyedMehdi <mehdibenayed97@gmail.com>
2024-05-03 03:46:25 +0200

    Njoura additions

--- EcoRebuild/src/components/LandingPageHeader/index.jsx
+++ EcoRebuild/src/components/LandingPageHeader/index.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { general } from "general";
 
+// TODO: Make more responsive
 const LandingPageHeader = (props) => {
   return (
     <>
