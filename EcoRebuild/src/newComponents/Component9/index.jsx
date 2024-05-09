bea71c2e1570c624de27bfcee6aa28c3a1a602ca
BenAyedMehdi <mehdibenayed97@gmail.com>
2024-05-09 02:46:49 +0200

    imporve how we started component

--- EcoRebuild/src/newComponents/Component9/index.jsx
+++ EcoRebuild/src/newComponents/Component9/index.jsx
@@ -14,13 +14,14 @@ const Component9 = () => {
                 className="text-4xl sm:text-[32px] md:text-[34px] text-gray-900 tracking-[-0.72px] w-full"
                 size="txtManropeExtraBold36"
               >
-                How it started
+                How we started
               </Text>
               <Text
                 className="leading-[180.00%] max-w-[531px] md:max-w-full text-gray-700 text-lg"
                 size="txtManropeRegular18Gray700"
               >
-                EcoRebuild emerged in response to the profound challenges of conflict and environmental degradation in Gaza. Inspired by the urgent need to repair and enhance the natural and built environments, our vision is to create a thriving community where ecological resilience and economic vitality are interwoven. The journey began in [start year], uniting community leaders, environmental experts, and local stakeholders.
+                EcoRebuild emerged in response to the profound challenges of
+                conflict and environmental degradation in Gaza.
               </Text>
             </div>
             <List
@@ -39,13 +40,14 @@ const Component9 = () => {
                     className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
                     size="txtManropeBold22"
                   >
-                    Genesis of the Vision
+                    Exploration phase
                   </Text>
                   <Text
                     className="text-gray-700"
                     size="txtManropeRegular18Gray700"
                   >
-                    Our initiative began with a clear mission to restore ecological resilience and revitalize the community through sustainable development.
+                    Participants engage in workshops designed to deepen their
+                    understanding of the unique challenges faced within Gaza.
                   </Text>
                 </div>
               </div>
@@ -61,13 +63,37 @@ const Component9 = () => {
                     className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
                     size="txtManropeBold22"
                   >
-                    Key Milestone
+                    Creation phase
                   </Text>
                   <Text
                     className="text-gray-700"
                     size="txtManropeRegular18Gray700"
                   >
-                    The journey officially began in [start year], marking the commencement of our efforts to transform the landscape and lives in Gaza.
+                    Guided by experienced mentors, participants transformed
+                    their understanding and insights into actionable ideas.
+                  </Text>
+                </div>
+              </div>
+              <div className="flex flex-1 sm:flex-col flex-row gap-[26px] items-start justify-start my-0 w-full">
+                <Text
+                  className="border-2 border-gray-900 border-solid flex h-[35px] items-center justify-center rounded-[17px] text-center text-gray-900 text-xl tracking-[-0.40px] w-[35px]"
+                  size="txtManropeExtraBold20"
+                >
+                  3
+                </Text>
+                <div className="flex flex-1 flex-col gap-[17px] items-start justify-start w-full">
+                  <Text
+                    className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
+                    size="txtManropeBold22"
+                  >
+                    Implementation phase
+                  </Text>
+                  <Text
+                    className="text-gray-700"
+                    size="txtManropeRegular18Gray700"
+                  >
+                    Teams transition from ideation to action, working closely
+                    with local communities to bring their solutions to life.
                   </Text>
                 </div>
               </div>
@@ -75,7 +101,7 @@ const Component9 = () => {
           </div>
           <Img
             className="flex-1 md:flex-none h-[25rem] sm:h-auto object-cover rounded-[10px] w-[20rem]"
-            src='ecorebuild_images/Hope-lamp.jpeg'
+            src="ecorebuild_images/Hope-lamp.jpeg"
             alt="Hope-lamp"
           />
         </div>
