3d4e2a62d4205a2d8311c4e088f339fbd27bee3d
BenAyedMehdi <mehdibenayed97@gmail.com>
2024-05-10 14:02:50 +0200

    cleanup footer ready for demo v1.0

--- EcoRebuild/src/components/LandingPageFooter/index.jsx
+++ EcoRebuild/src/components/LandingPageFooter/index.jsx
@@ -11,7 +11,7 @@ const LandingPageFooter = () => {
         <div className="flex flex-col md:gap-10 gap-[20px] items-start justify-start w-full">
           <div className="flex md:flex-col flex-row md:gap-5 items-center justify-start w-full">
             <div className="flex flex-col gap-[23px] items-start justify-start w-[341px]">
-              <div className="flex flex-row gap-[11px] h-10 md:h-auto items-center justify-start w-[341px]">
+              <div className="flex flex-row gap-[11px] md:h-auto items-center justify-start w-[341px]">
                 <Img className="h-[60px] w-[70px]" src={general.logo} alt="home_One" />
                 <Text
                   className="text-red-600 text-3xl w-auto"
@@ -20,7 +20,7 @@ const LandingPageFooter = () => {
                   <Link to="/">{general.name}</Link>
                 </Text>
               </div>
-              <div className="flex flex-col gap-7 h-[194px] md:h-auto items-start justify-start w-full">
+              <div className="flex flex-col gap-7 md:h-auto items-start justify-start w-full">
                 <Text
                   className="leading-[160.00%] text-base text-gray-900"
                   size="txtManropeSemiBold16"
@@ -41,65 +41,16 @@ const LandingPageFooter = () => {
                     {general.email}
                   </Text>
                 </div>
-                <div className="flex flex-row gap-3 items-start justify-start w-full">
-                  {/* <a target="_blank" href={general.facebook}>
-                    <div className="flex flex-col h-[30px] items-center justify-start w-[30px]">
-                      <Img
-                        className="h-[30px] w-[30px]"
-                        src="images/img_plus.svg"
-                        alt="plus"
-                      />
-                    </div>
-                  </a> */}
-                </div>
               </div>
             </div>
-            <div className="flex flex-1 md:flex-col flex-row md:gap-10 gap-[77px] items-start justify-start w-full ml-8">
-              {/* <div className="flex flex-1 flex-col gap-5 items-start justify-start w-full">
-                <Text
-                  className="text-gray-900 text-lg w-full"
-                  size="txtManropeBold18Gray900"
-                >
-                  <Link to="/services">Services</Link>
-                </Text>
-                <div className="flex flex-col gap-3.5 items-start justify-start w-full">
-                  <Text
-                    className="text-base text-gray-900 w-full"
-                    size="txtManropeSemiBold16"
-                  >
-                    Websites
-                  </Text>
-                  <Text
-                    className="text-base text-gray-900 w-full"
-                    size="txtManropeSemiBold16"
-                  >
-                    Mobile Apps 
-                  </Text>
-                  <Text
-                    className="text-base text-gray-900 w-full"
-                    size="txtManropeSemiBold16"
-                  >
-                    Desktop apps
-                  </Text>
-                </div>
-              </div> */}
+            <div className="flex flex-1 md:flex-col flex-row md:gap-10 gap-[77px] items-start justify-start w-full ml-20">
               <div className="flex flex-1 flex-col gap-5 items-start justify-start w-full">
                 <Text
                   className="text-gray-900 text-lg w-full"
                   size="txtManropeBold18Gray900"
                 >
-                  <Link to="/portfolio">Projects</Link>
+                  <Link to="/">Home</Link>
                 </Text>
-                {/* <div className="flex flex-col gap-3.5 items-start justify-start w-full">
-                  
-                  <Text
-                    className="text-base text-gray-900 w-full"
-                    size="txtManropeSemiBold16"
-                  >
-                    My experience
-                  </Text>
-                  
-                </div> */}
               </div>
               {/* About section */}
               <div className="flex flex-1 flex-col gap-5 items-start justify-start w-full">
@@ -109,7 +60,7 @@ const LandingPageFooter = () => {
                 >
                   <Link to="/about">About</Link>
                 </Text>
-                <div className="flex flex-col gap-3.5 items-start justify-start w-full">
+                {/* <div className="flex flex-col gap-3.5 items-start justify-start w-full">
                   <Text
                     className="text-base text-gray-900 w-full"
                     size="txtManropeSemiBold16"
@@ -122,7 +73,7 @@ const LandingPageFooter = () => {
                   >
                     <Link to="/about">Adventures</Link>
                   </Text>
-                </div>
+                </div> */}
               </div>
               <div className="flex flex-1 flex-col gap-5 items-start justify-start w-full">
                 <Text
@@ -131,14 +82,14 @@ const LandingPageFooter = () => {
                 >
                   <Link to="/contactpage">Contact</Link>
                 </Text>
-                <div className="flex flex-col gap-3.5 items-start justify-start w-full">
+                {/* <div className="flex flex-col gap-3.5 items-start justify-start w-full">
                   <Text
                     className="text-base text-gray-900 w-full"
                     size="txtManropeSemiBold16"
                   >
                     Partnership
                   </Text>
-                </div>
+                </div> */}
               </div>
               <div className="flex flex-1 flex-col gap-5 items-start justify-start w-full">
                 <Text
