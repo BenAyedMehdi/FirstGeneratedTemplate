f5100c16b4a4b7b9ffa97b6eaaedfe1f88fd1e41
BenAyedMehdi <mehdibenayed97@gmail.com>
2024-06-09 03:34:37 +0200

    chore: Add react-toastify for displaying toast notifications for email sending

--- MehdiPortfolio/src/App.jsx
+++ MehdiPortfolio/src/App.jsx
@@ -1,10 +1,13 @@
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import Routes from "./Routes";
+import {ToastContainer} from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 
 function App() {
   return (
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <ToastContainer/>
       <Routes />
     </GoogleOAuthProvider>
   );
--- MehdiPortfolio/src/newComponents/Component21/index.jsx
+++ MehdiPortfolio/src/newComponents/Component21/index.jsx
@@ -1,41 +1,43 @@
-import React, {useRef} from "react";
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { Button, Img, Text, Line, Input } from "components";
 import { general } from "general";
+import { toast } from "react-toastify";
 
 const Component21 = () => {
-
   const form = useRef();
 
-   const sendEmail = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs
-    // SERVICE_ID:service_38xdw0x
-    // TEMPLATE_ID:template_to9p96q
-      .sendForm('service_38xdw0x', 'template_to9p96q', form.current, {
-    // PUBLIC_KEY:oh5f-_PYYg0KKG4ua
-        publicKey: 'oh5f-_PYYg0KKG4ua',
+    const SERVICE_ID= "service_i8vm329";
+    const TEMPLATE_ID= "template_ufp5dmt";
+    const PUBLIC_KEY= "IOkGoHGi_AhtcUzet";
+    console.log(form.current)
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, {
+        publicKey: PUBLIC_KEY,
       })
       .then(
         () => {
-          console.log('SUCCESS!');
+          toast.success("Email sent successfully!", {
+            theme: "colored",
+          });
+          console.log("SUCCESS!");
         },
         (error) => {
-          console.log('FAILED...', error.text);
-        },
+          toast.error("Failed to send email!", {
+            theme: "colored",
+          });
+          console.log("FAILED...", error.text);
+        }
       );
-      e.target.reset();
+    e.target.reset();
   };
-
 
   return (
     <>
       <div className="flex flex-col font-manrope items-center justify-start md:px-10 sm:px-5 px-[120px] w-full">
         <div className="flex flex-col gap-11 items-center justify-start max-w-[1200px] mx-auto w-full">
-          
-
-        <div className="flex flex-col gap-4 items-center justify-start w-full">
+          <div className="flex flex-col gap-4 items-center justify-start w-full">
             <Text
               className="sm:text-[40px] md:text-[46px] text-[54px] text-center text-gray-900 tracking-[-1.08px] w-full"
               size="txtManropeExtraBold54"
@@ -63,10 +65,9 @@ const Component21 = () => {
           </div>
 
           <div className="bg-white-A700 border border-bluegray-100 border-solid flex md:flex-col flex-row gap-[50px] items-start justify-start p-[50px] md:px-10 sm:px-5 rounded-[10px] w-full">
-            
             <div className="flex flex-1 flex-col gap-10 items-start pl-10 justify-center w-full">
               <div className="flex flex-col gap-8 items-start justify-start w-full">
-                <br/> 
+                <br />
                 <div className="flex flex-col gap-1 items-start mt-4 justify-start w-full">
                   <Text
                     className="text-gray-900 text-2xl tracking-[-0.40px] w-auto"
@@ -115,9 +116,9 @@ const Component21 = () => {
               </div>
               <div className="flex flex-col gap-[13px] items-start justify-start w-full sm:w-full">
                 <Text
-                    className="text-gray-900 text-2xl tracking-[-0.40px] w-auto"
-                    size="txtManropeExtraBold28"
-                  >
+                  className="text-gray-900 text-2xl tracking-[-0.40px] w-auto"
+                  size="txtManropeExtraBold28"
+                >
                   Social
                 </Text>
                 <div className="flex flex-row gap-4 items-start justify-start w-full">
@@ -145,85 +146,91 @@ const Component21 = () => {
                 </div>
               </div>
               <Text
-                    className=" md:text-[22px] text-center text-gray-900 sm:text-xl tracking-[-0.48px] w-full"
-                    size="txtManropeBold24Gray900"
-                  ></Text>
-              <a target="_blank" href={require("../../assets/resume.pdf")}  className="cursor-pointer w-full">
-              <Button className="bg-gray-200  text-xl font-semibold py-[17px] rounded-[10px] text-center  text-black w-full">
-                View CV
-
-              </Button>
+                className=" md:text-[22px] text-center text-gray-900 sm:text-xl tracking-[-0.48px] w-full"
+                size="txtManropeBold24Gray900"
+              ></Text>
+              <a
+                target="_blank"
+                href={require("../../assets/resume.pdf")}
+                className="cursor-pointer w-full"
+              >
+                <Button className="bg-gray-200  text-xl font-semibold py-[17px] rounded-[10px] text-center  text-black w-full">
+                  View CV
+                </Button>
               </a>
             </div>
             <Line className="bg-bluegray-100 h-[534px] md:h-px md:w-full w-px" />
-            <div className="flex flex-1 flex-col gap-10 items-start justify-start w-full">
-              <div className="flex flex-col gap-6 items-start justify-start w-full">
-                <Text
-                  className="sm:text-2xl md:text-[26px] text-[28px] text-gray-900 tracking-[-0.56px] w-full"
-                  size="txtManropeExtraBold28"
-                >
-                  Send a message
-                </Text>
-                <div className="flex flex-col gap-3 items-start justify-start w-full">
-                  <Input
-                    name="textfieldlarge"
-                    placeholder="Full Name"
-                    className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg w-full"
-                    wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex pl-4 pr-[35px] py-[17px] rounded-[10px] w-full"
-                    type="text"
-                    prefix={
-                      <Img
-                        className="mt-auto mb-px h-6 mr-3.5"
-                        src="images/img_user.svg"
-                        alt="user"
-                      />
-                    }
-                  ></Input>
-                  <Input
-                    name="textfieldlarge_One"
-                    placeholder="Email Address"
-                    className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg w-full"
-                    wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex pl-4 pr-[35px] py-[17px] rounded-[10px] w-full"
-                    type="email"
-                    prefix={
-                      <Img
-                        className="mt-auto mb-px h-6 mr-3.5"
-                        src="images/img_mail_gray_600_24x24.svg"
-                        alt="mail"
-                      />
-                    }
-                  ></Input>
-                  <Input
-                    name="textfieldlarge_Two"
-                    placeholder="Phone Number"
-                    className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg w-full"
-                    wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex pl-4 pr-[35px] py-[17px] rounded-[10px] w-full"
-                    type="number"
-                    prefix={
-                      <Img
-                        className="mt-auto mb-px h-6 mr-3.5"
-                        src="images/img_call.svg"
-                        alt="call"
-                      />
-                    }
-                  ></Input>
-                  <div className="bg-white-A700 border border-bluegray-100 border-solid flex flex-col h-[152px] md:h-auto items-start justify-start px-[19px] py-3.5 rounded-[10px] w-full">
-                    <Text
-                      className="text-gray-600 text-lg w-auto"
-                      size="txtManropeSemiBold18Gray600"
-                    >
-                      Message
-                    </Text>
+            <div className="flex flex-1 flex-col items-center justify-start w-full py-10">
+              <form ref={form} onSubmit={sendEmail} className="w-full max-w-lg">
+                <div className="flex flex-col gap-4">
+                  <Text
+                    className="text-2xl md:text-3xl text-gray-900 font-bold tracking-tight"
+                    size="txtManropeExtraBold28"
+                  >
+                    Send a message
+                  </Text>
+                  <div className="flex flex-col gap-2">
+                    <Input
+                      name="user_name"
+                      placeholder="Full Name"
+                      className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg"
+                      wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex items-center pl-4 pr-[35px] py-[17px] rounded-[10px] "
+                      type="text"
+                      required
+                      prefix={
+                        <Img
+                          className="h-5 mr-3"
+                          src="images/img_user.svg"
+                          alt="user"
+                        />
+                      }
+                    ></Input>
+                    <Input
+                      name="user_email"
+                      placeholder="Email Address"
+                      className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg"
+                      wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex items-center pl-4 pr-[35px] py-[17px] rounded-[10px]"
+                      type="email"
+                      required
+                      prefix={
+                        <Img
+                          className="h-5 mr-3"
+                          src="images/img_mail_gray_600_24x24.svg"
+                          alt="mail"
+                        />
+                      }
+                    ></Input>
+                    <Input
+                      name="user_phone_number"
+                      placeholder="Phone Number"
+                      className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg "
+                      wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex items-center pl-4 pr-[35px] py-[17px] rounded-[10px] "
+                      type="text"
+                      required
+                      prefix={
+                        <Img
+                          className="h-5 mr-3"
+                          src="images/img_call.svg"
+                          alt="call"
+                        />
+                      }
+                    ></Input>
+                    <Input
+                      name="message"
+                      className="font-semibold placeholder:text-gray-600 text-gray-600 text-lg w-full"
+                      placeholder="Message"
+                      wrapClassName="bg-white border border-gray-200 rounded-lg p-4 h-48"
+                      type="text"
+                      required
+                    ></Input>
                   </div>
                 </div>
-              </div>
-              <Button className="bg-gray-900 cursor-pointer font-semibold py-[17px] rounded-[10px] text-base text-center text-white-A700 w-full">
-                SEND
-              </Button>
+                <Button className="mt-4 bg-gray-900 font-semibold py-4 rounded-lg text-white-A700 w-full text-lg hover:bg-gray-700 transition-colors duration-300">
+                  SEND
+                </Button>
+              </form>
             </div>
-            
           </div>
-
         </div>
       </div>
     </>
